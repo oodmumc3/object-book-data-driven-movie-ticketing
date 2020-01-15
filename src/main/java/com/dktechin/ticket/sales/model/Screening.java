c64e301591e28496ee3cc9a1706b654c239200c1
jin.bak <jin.bak@kakaocorp.com>
2020-01-15 10:43:30 +0900

    인원수별 예매 금액 계산 추가

--- src/main/java/com/dktechin/ticket/sales/model/Screening.java
+++ src/main/java/com/dktechin/ticket/sales/model/Screening.java
@@ -21,5 +21,29 @@ public class Screening {
 
     public void setSequence(int sequence) { this.sequence = sequence; }
     public void setWhenScreened(LocalDateTime whenScreened) { this.whenScreened = whenScreened; }
+
+    /**
+     * 예매 금액을 계산하여 반환한다.
+     * @param audienceCount 예매 인원수
+     * @return 총 예매 금액
+     */
+    public Money calculateFee(int audienceCount) {
+        switch (this.movie.getMovieType()) {
+            case AMOUNT_DISCOUNT:
+                if (this.movie.isDiscountable(this.whenScreened, this.sequence)) {
+                    return this.movie.calculateAmountDiscountedFee().times(audienceCount);
+                }
+
+            case PERCENT_DISCOUNT:
+                if (this.movie.isDiscountable(this.whenScreened, this.sequence)) {
+                    return this.movie.calculatePercentDiscountedFee().times(audienceCount);
+                }
+
+            case NONE_DISCOUNT:
+                return this.movie.calculateNoneDiscountedFee().times(audienceCount);
+        }
+
+        return this.movie.calculateNoneDiscountedFee().times(audienceCount);
+    }
 }
 
