22fff58314b53a4691f481b892bc7840a1d1d418
jin.bak <jin.bak@kakaocorp.com>
2020-01-15 10:37:20 +0900

    할인 가능 여부 메서드 추가

--- src/main/java/com/dktechin/ticket/sales/model/Movie.java
+++ src/main/java/com/dktechin/ticket/sales/model/Movie.java
@@ -1,7 +1,9 @@
 package com.dktechin.ticket.sales.model;
 
+import com.dktechin.ticket.sales.enums.DiscountConditionType;
 import com.dktechin.ticket.sales.enums.MovieType;
 import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -49,4 +51,64 @@ public class Movie {
 
     public double getDiscountPercent() { return discountPercent; }
     public void setDiscountPercent(double discountPercent) { this.discountPercent = discountPercent; }
+
+    /**
+     * 비용 할인 금액을 제외하고 예매금액을 계산하여 반환한다.
+     * @return 할인 예매 금액
+     */
+    public Money calculateAmountDiscountedFee() {
+        if (movieType != MovieType.AMOUNT_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+
+        return this.fee.minus(this.discountAmount);
+    }
+
+    /**
+     * 비율 기준 할인 금액을 제외하고 예매금액을 계산하여 반환한다.
+     * @return 할인 예매 금액
+     */
+    public Money calculatePercentDiscountedFee() {
+        if (movieType != MovieType.PERCENT_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+
+        return this.fee.minus(this.fee.times(this.discountPercent));
+    }
+
+    /**
+     * 할인 정책이 없으므로 예매금액을 그대로 반환한다.
+     * @return 할인 예매 금액
+     */
+    public Money calculateNoneDiscountedFee() {
+        if (movieType != MovieType.NONE_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+
+        return this.fee;
+    }
+
+    /**
+     * 할인이 가능한 예매인지 판단하여 반환한다.
+     * @param whenScreened 예매 상영 시간
+     * @param sequence 예매 상영 회차
+     * @return 할인 가능 여부
+     */
+    public boolean isDiscountable(LocalDateTime whenScreened, int sequence) {
+        for (DiscountCondition discountCondition : this.discountConditions) {
+            if (discountCondition.getType() == DiscountConditionType.PERIOD) {
+                // 기간 할인 판단
+                if (discountCondition.isDiscountable(whenScreened.getDayOfWeek(), whenScreened.toLocalTime())) {
+                    return true;
+                }
+            } else {
+                // 회차 할인 판단
+                if (discountCondition.isDiscountable(sequence)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
 }
